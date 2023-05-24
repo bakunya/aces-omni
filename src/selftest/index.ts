4866f261e41a5d70ceff1e7c460e3952b3f0e7bf
muslax <muslax@gmail.com>
2023-05-24 10:15:49 +0700

    update selftest index

--- src/selftest/index.ts
+++ src/selftest/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { acesid, serveAsset, validReferer } from "../utils";
+import { acesid, validReferer } from "../utils";
 import { getTestUser } from "../session";
 import Abstract from "./abstract";
 import AIME from "./aime";
@@ -29,22 +29,22 @@ export const modules: Record<string, Module> = {
 const selftest = new Hono<{ Bindings: Env }>()
 
 selftest.use("*", async (c, next) => {
-  const user = await getTestUser(c)
-  if (!user) return c.json({ info: '401 Unauthorized' }, 401)
+  const u = await getTestUser(c)
+  if (!u) return c.json({ info: '401 Unauthorized' }, 401)
   await next()
 })
 
 selftest.get("/", async (c) => {
-  const user = await getTestUser(c)
+  const u = await getTestUser(c)
 
-  const userTests: string[] = []
-  Object.entries(user?.tests).forEach(([type, ]) => {
-    userTests.push(
-      ` -  <a style="color:blue;" href="/selftest/${user?.pid}/${modules[type].slug}">${modules[type].name}</a><br/>`
+  const tests: string[] = []
+  Object.entries(u?.tests).forEach(([type, ]) => {
+    tests.push(
+      ` -  <a style="color:blue;" href="/selftest/${u?.pid}/${modules[type].slug}">${modules[type].name}</a><br/>`
     )
   })
   const html = `<p style="font-family:monospace;line-height:1.5">
-  DAFTAR TES ANDA<br/>${userTests.join('')}</p>`
+  DAFTAR TES ANDA<br/>${tests.join('')}</p>`
   return c.html(html)
 })
 
@@ -64,32 +64,32 @@ selftest.get("/:pid/:test_type", async (c) => {
   ////////////////////////////////////////////
   // Check if user has the test
   ////////////////////////////////////////////
-  const user = await getTestUser(c) as Persona
-  if (!user.tests[type]) return c.text('Unauthorized', 401)
+  const u = await getTestUser(c) as Persona
+  if (!u.tests[type]) return c.text('Unauthorized', 401)
 
   ////////////////////////////////////////////
   // Check if userdata been created
   ////////////////////////////////////////////
   const table = type + "_userdata"
   const sql = `SELECT * FROM ${table} WHERE uid=? AND pid=?`;
-  const found: any = await c.env.DB.prepare(sql).bind(user.id, user.pid).first();
+  const found: any = await c.env.DB.prepare(sql).bind(u.id, u.pid).first();
   console.log("found", found)
 
   let rowid;
   if (!found) {
     rowid = acesid()
     const ts = new Date().getTime();
-    const version = user.tests[type];
+    const version = u.tests[type];
 
     if (type == "gmate") {
       const sequence = "TBD"
       const cols = "id, uid, pid, version, enter, sequence";
       const sql = `INSERT INTO ${table} (${cols}) VALUES (?,?,?,?,?,?)`;
-      await c.env.DB.prepare(sql).bind(rowid, user.id, user.pid, version, ts, sequence).run();
+      await c.env.DB.prepare(sql).bind(rowid, u.id, u.pid, version, ts, sequence).run();
     } else {
       const cols = "id, uid, pid, version, enter";
       const sql = `INSERT INTO ${table} (${cols}) VALUES (?,?,?,?,?)`;
-      await c.env.DB.prepare(sql).bind(rowid, user.id, user.pid, version, ts).run();
+      await c.env.DB.prepare(sql).bind(rowid, u.id, u.pid, version, ts).run();
     }
   } else {
     rowid = found.id
@@ -107,20 +107,20 @@ selftest.get("/:pid/:test_type/:rowid", async (c) => {
   const type = c.req.param("test_type")
   const rowid = c.req.param("rowid")
 
-  const p = await getTestUser(c) as Persona
+  const u = await getTestUser(c) as Persona
 
   if (!modules[type]) return c.notFound()
   if (!validReferer(c)) return c.text("Missing referer", 401);
-  if (pid != p.pid) return c.text('401 Unauthorized', 401);
+  if (pid != u.pid) return c.text('401 Unauthorized', 401);
 
-  if (type == "abstract")   return await Abstract.index(c, p, rowid);
-  if (type == "numerical")  return await Numerical.index(c, p, rowid);
-  if (type == "verbal")     return await Verbal.index(c, p, rowid);
-  if (type == "aime")       return await AIME.index(c, p, rowid);
-  if (type == "csi")        return await CSI.index(c, p, rowid);
-  if (type == "gmate")      return await GMATE.index(c, p, rowid);
-  if (type == "gpq")        return await GPQ.index(c, p, rowid);
-  if (type == "gpro")       return await GPRO.index(c, p, rowid);
+  if (type == "abstract")   return await Abstract.index(c, u, rowid);
+  if (type == "numerical")  return await Numerical.index(c, u, rowid);
+  if (type == "verbal")     return await Verbal.index(c, u, rowid);
+  if (type == "aime")       return await AIME.index(c, u, rowid);
+  if (type == "csi")        return await CSI.index(c, u, rowid);
+  if (type == "gmate")      return await GMATE.index(c, u, rowid);
+  if (type == "gpq")        return await GPQ.index(c, u, rowid);
+  if (type == "gpro")       return await GPRO.index(c, u, rowid);
 
   return c.notFound()
 })
@@ -128,18 +128,18 @@ selftest.get("/:pid/:test_type/:rowid", async (c) => {
 // Test post
 selftest.post("/:test_type", async (c) => {
   const type = c.req.param("test_type")
-  const p = await getTestUser(c) as Persona
+  const u = await getTestUser(c) as Persona
 
   if (!modules[type]) return c.notFound()
 
-  if (type == "abstract")   return await Abstract.post(c, p);
-  if (type == "numerical")  return await Numerical.post(c, p);
-  if (type == "verbal")     return await Verbal.post(c, p);
-  if (type == "aime")       return await AIME.post(c, p);
-  if (type == "csi")        return await CSI.post(c, p);
-  if (type == "gmate")      return await GMATE.post(c, p);
-  if (type == "gpq")        return await GPQ.post(c, p);
-  if (type == "gpro")       return await GPRO.post(c, p);
+  if (type == "abstract")   return await Abstract.post(c, u);
+  if (type == "numerical")  return await Numerical.post(c, u);
+  if (type == "verbal")     return await Verbal.post(c, u);
+  if (type == "aime")       return await AIME.post(c, u);
+  if (type == "csi")        return await CSI.post(c, u);
+  if (type == "gmate")      return await GMATE.post(c, u);
+  if (type == "gpq")        return await GPQ.post(c, u);
+  if (type == "gpro")       return await GPRO.post(c, u);
 })
 
 export { selftest }
