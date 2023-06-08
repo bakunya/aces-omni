1ab435c9fb404d3164fed3d5a8d47199d35c721d
irvan hakim <irvanhakim.dev@gmail.com>
2023-06-08 19:56:39 +0700

    init --irvan-hakim

---
+++ schema/migrate-local.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo "START MIGRATION";
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/aces.sql;
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/modules.sql;
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/userdata1.sql;
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/userdata2.sql;
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/userdata3.sql;
+npx wrangler d1 execute acesdb --local --file=./schema/20230523/v202303.sql;
+echo "END MIGRATION";
